fcb55ca69fdf7385959a3daecf2bb9b62aa9c8ca
Teng Zhang <ztnuaa0211@gmail.com>
2019-02-24 21:07:49 +0800

    Finish debug

---
+++ SpotifyLyrics.py
@@ -0,0 +1,149 @@
+
+import sys
+import time
+import spotipy
+import spotipy.util as util
+import services as s
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+name = config['client']['name']
+client_id = config['client']['client_id']
+client_secret = config['client']['client_secret']
+scope = config['oauth']['scope']
+redirect_uri = config['oauth']['redirect_uri']
+print(name)
+print(client_id)
+print(client_secret)
+print(scope)
+print(redirect_uri)
+token = util.prompt_for_user_token(
+        username=name,
+        scope=scope,
+        client_id=client_id,
+        client_secret=client_secret,
+        redirect_uri=redirect_uri)
+
+is_daemon = True
+lag_time = 1.0 # s
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+
+    result = sp.current_playback()
+
+    if result is not None:
+
+        is_playing = result['is_playing']
+
+        progress_ms = result['progress_ms']
+
+        device = result['device']['name'] # current playing device
+
+        song = result['item']['name'] # song name
+
+        song_id = result['item']['id'] # song's unique id
+
+        artist = result['item']['artists'][0]['name'] # only get the first artist
+
+        album = result['item']['album']['name'] # alubm name
+
+        # convert the Full width quote to Half width quote
+        # in my case, Taylor Swift's "Don't blame me" was "Don’t blame me", so the minilyrics can't find its lyrics
+        song = song.replace('’','\'')
+        artist = artist.replace('’','\'')
+
+        # get lyrics with timestamps from MiniLyrics
+        lyrics, url, service_name, timed = s._minilyrics(artist, song)
+        # error message in lyrics if can't get lyrics
+        error = "Error: Could not find lyrics."
+
+        if lyrics != error:
+            timeline, lrc = s.lyricSplit(lyrics)
+
+        while (is_daemon):
+            # update status because search lyrics may take a while
+            result = sp.current_playback()
+
+            is_playing = result['is_playing']
+
+            progress_ms = result['progress_ms']
+
+            print('\033[2J\033[2;0H\t♫ \033[1;35m {0} \033[0m - \033[1;32m {1} \033[0m'.format(artist,song))
+            while is_playing:
+                if (is_daemon == False):
+                    # turn off lyrics
+                    break
+                if lyrics == error:
+                    # only show the error message
+                    current_line = " >: Don't have lyrics right now :< "
+                    print('\033[4;0H\033[K\t \033[1;36m {}\033[0m'.format(current_line))
+                    sleep_time=30.0
+                    time.sleep(sleep_time)
+                    break
+                else:
+                    if is_playing:
+                        for n in range(1,len(timeline)):
+                            if progress_ms <= timeline[n] and progress_ms >= timeline[n-1]:
+                                #print(progress_ms, lrc[n-1])
+                                current_line = lrc[n-1]
+                                print('\033[4;0H\033[K\t \033[1;36m {}\033[0m'.format(current_line))
+                                # sleep a while [s]
+                                sleep_time = abs(timeline[n]-progress_ms)/1000.0+lag_time
+                                time.sleep(sleep_time)
+                                print('\033[10;0H\033[?25h')
+                                break
+                    else:
+                        current_line = " >: Paused :<"
+                        print('\033[4;0H\033[K\t \033[1;36m {}\033[0m'.format(current_line))
+                        # if paused, request api every 2.0s to update status
+                        sleep_time=60.0
+                        time.sleep(sleep_time)
+
+                # update playback status
+                result = sp.current_playback()
+                if result is not None:
+                    is_playing = result['is_playing']
+                    progress_ms = result['progress_ms']
+                    song = result['item']['name'] # song name
+                    _song_id = result['item']['id']
+                    artist = result['item']['artists'][0]['name'] # only get the first artist
+
+                    # is still playing this song or not?
+                    if is_playing:
+                        if _song_id != song_id:
+                            # refresh screen
+                            print('\033[2J\033[2;0H\t♫ \033[1;35m {0} \033[0m - \033[1;32m {1} \033[0m'.format(artist,song))
+
+                            lyrics, url, service_name, timed = s._minilyrics(artist,song)
+                            if lyrics != error:
+                                timeline, lrc = s.lyricSplit(lyrics)
+                            song_id = _song_id
+                    else:
+                        break
+                else:
+                    # result is None, Spotify Client is not opened
+                    break
+            if (is_daemon):
+                if result is not None:
+                    if not is_playing:
+                        # paused but still opened the Spotify client
+                        current_line = " >: Paused :< "
+                        print('\033[4;0H\033[K\t \033[1;36m {}\033[0m'.format(current_line))
+                        time.sleep(10.0)
+                    else:
+                        # Spotify is not opened, stop the daemon
+                        break
+            else:
+                current_line = ""
+                break
+    else:
+        current_line = " >: Spotify is sleeping :< "
+        print('\033[4;0H\033[K\t \033[1;36m {}\033[0m'.format(current_line))
+
+
+else:
+    current_line =" Can't get token for "+name
+    print('\033[4;0H\033[K\t \033[1;36m {}\033[0m'.format(current_line))
---
+++ services.py
@@ -0,0 +1,219 @@
+from bs4 import BeautifulSoup
+import requests
+import urllib
+import re
+import unidecode # to remove accents
+import codecs
+import lyrics as minilyrics
+
+error = "Error: Could not find lyrics."
+proxy = urllib.request.getproxies()
+
+def lyricSplit(lyrics):
+    timeline = []
+    lrc = []
+    for line in lyrics.splitlines():
+        if line.startswith(("[0","[1","[2","[3")):
+            firstLine = True
+            regex = re.compile('\[.+?\]')
+            _time = re.match(regex,line).group(0)
+            _time = _time[1:len(_time)-1]
+            line = regex.sub('',line)
+            # remove blank space
+            line = line.strip()
+            if line != '':
+                # convert _time format from min:s.ms to ms
+                i = _time.index(':')
+                j = _time.index('.')
+                _min = int(_time[0:i])
+                _s = int(_time[i+1:j])
+                _ms = int(_time[j+1:])
+                time = _min*60*1000+_s*1000+_ms*10
+                timeline.append(time)
+                lrc.append(line)
+    return(timeline, lrc)
+
+def _minilyrics(artist, song):
+    service_name = "Mini Lyrics"
+    url = ""
+    timed = False
+    try:
+        data = minilyrics.MiniLyrics(artist, song)
+        for item in data:
+            if item['url'].endswith(".lrc"):
+                url = item['url']
+                break
+        lyrics = requests.get(url, proxies=proxy).text
+        timed = True
+    except Exception:
+        lyrics = error
+    if url == "":
+        lyrics = error
+    if artist.lower().replace(" ", "") not in lyrics.lower().replace(" ", ""):
+        lyrics = error
+        timed = False
+
+    return(lyrics, url, service_name, timed)
+
+def _wikia(artist, song):
+    service_name = "Wikia"
+    url = ""
+    try:
+        lyrics = minilyrics.LyricWikia(artist, song)
+        url = "http://lyrics.wikia.com/%s:%s" % (artist.replace(' ', '_'), song.replace(' ', '_'))
+    except Exception:
+        lyrics = error
+    if "TrebleClef.png" in lyrics:
+        lyrics = "(Instrumental)"
+    if "Instrumental" in lyrics:
+        lyrics = "(Instrumental)"
+    if lyrics == "error":
+        lyrics = error
+    return(lyrics, url, service_name)
+
+def _musixmatch(artist, song):
+    service_name = "Musixmatch"
+    url = ""
+    try:
+        searchurl = "https://www.musixmatch.com/search/%s-%s/tracks" % (artist.replace(' ', '-'), song.replace(' ', '-'))
+        header = {"User-Agent":"curl/7.9.8 (i686-pc-linux-gnu) libcurl 7.9.8 (OpenSSL 0.9.6b) (ipv6 enabled)"}
+        searchresults = requests.get(searchurl, headers=header, proxies=proxy)
+        soup = BeautifulSoup(searchresults.text, 'html.parser')
+        page = re.findall('"track_share_url":"([^"]*)', soup.text)
+        url = codecs.decode(page[0], 'unicode-escape')
+        lyricspage = requests.get(url, headers=header, proxies=proxy)
+        soup = BeautifulSoup(lyricspage.text, 'html.parser')
+        lyrics = soup.text.split('"body":"')[1].split('","language"')[0]
+        lyrics = lyrics.replace("\\n", "\n")
+        lyrics = lyrics.replace("\\", "")
+        if lyrics.strip() == "":
+            lyrics = error
+    except Exception:
+        lyrics = error
+    return(lyrics, url, service_name)
+
+def _songmeanings(artist, song):
+    service_name = "Songmeanings"
+    url = ""
+    try:
+        searchurl = "http://songmeanings.com/m/query/?q=%s %s" % (artist, song)
+        searchresults = requests.get(searchurl, proxies=proxy)
+        soup = BeautifulSoup(searchresults.text, 'html.parser')
+        url = ""
+        for link in soup.find_all('a', href=True):
+            if "songmeanings.com/m/songs/view/" in link['href']:
+                url = "https:" + link['href']
+                break
+            elif "/m/songs/view/" in link['href']:
+                result = "http://songmeanings.com" + link['href']
+                lyricspage = requests.get(result, proxies=proxy)
+                soup = BeautifulSoup(lyricspage.text, 'html.parser')
+                url = "http://songmeanings.com" + link['href'][2:]
+                break
+            else:
+                pass
+        templyrics = soup.find_all("li")[4]
+        lyrics = templyrics.getText()
+        lyrics = lyrics.split("(r,s)};})();")[1]
+    except Exception:
+        lyrics = error
+    if lyrics == "We are currently missing these lyrics.":
+        lyrics = error
+
+    #lyrics = lyrics.encode('cp437', errors='replace').decode('utf-8', errors='replace')
+    return(lyrics, url, service_name)
+
+def _songlyrics(artist, song):
+    service_name = "Songlyrics"
+    url = ""
+    try:
+        artistm = artist.replace(" ", "-")
+        songm = song.replace(" ", "-")
+        url = "http://www.songlyrics.com/%s/%s-lyrics" % (artistm, songm)
+        lyricspage = requests.get(url, proxies=proxy)
+        soup = BeautifulSoup(lyricspage.text, 'html.parser')
+        lyrics = soup.find(id="songLyricsDiv").get_text()
+    except Exception:
+        lyrics = error
+    if "Sorry, we have no" in lyrics:
+        lyrics = error
+    if "We do not have" in lyrics:
+        lyrics = error
+    return(lyrics, url, service_name)
+
+
+def _genius(artist, song):
+    service_name = "Genius"
+    url = ""
+    try:
+        url = "http://genius.com/%s-%s-lyrics" % (artist.replace(' ', '-'), song.replace(' ', '-'))
+        lyricspage = requests.get(url, proxies=proxy)
+        soup = BeautifulSoup(lyricspage.text, 'html.parser')
+        lyrics = soup.find("div", {"class": "lyrics"}).get_text()
+        if artist.lower().replace(" ", "") not in soup.text.lower().replace(" ", ""):
+            lyrics = error
+    except Exception:
+        lyrics = error
+    return(lyrics, url, service_name)
+
+def _versuri(artist, song):
+    service_name = "Versuri"
+    url = ""
+    try:
+        searchurl = "http://www.versuri.ro/q/%s+%s/" % (artist.replace(" ", "+"), song.replace(" ", "+"))
+        searchresults = requests.get(searchurl, proxies=proxy)
+        soup = BeautifulSoup(searchresults.text, 'html.parser')
+        for x in soup.findAll('a'):
+            if "/versuri/" in x['href']:
+                url = "http://www.versuri.ro" + x['href']
+                break
+            else:
+                pass
+        if url is "":
+            lyrics = error
+        else:
+            lyricspage = requests.get(url, proxies=proxy)
+            soup = BeautifulSoup(lyricspage.text, 'html.parser')
+            content = soup.find_all('div',{'id':'pagecontent'})[0]
+            lyrics = str(content)[str(content).find("</script><br/>") + 14:str(content).find("<br/><br/><center>")]
+            lyrics = lyrics.replace("<br/>", "")
+    except Exception:
+        lyrics = error
+    return(lyrics, url, service_name)
+
+# tab/chord services
+
+def _ultimateguitar(artist, song):
+    artist = unidecode.unidecode(artist)
+    song = unidecode.unidecode(song)
+    url_pt1 = 'https://www.ultimate-guitar.com/search.php?view_state=advanced&band_name='
+    url_pt2 = '&song_name='
+    url_pt3 = '&type%5B%5D=300&type%5B%5D=200&rating%5B%5D=5&version_la='
+    # song = song.replace('-', '+')
+    # artist = artist.replace('-', '+')
+    url = url_pt1+artist+url_pt2+song+url_pt3
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    urls = []
+    for a in soup.find_all(class_='song result-link js-search-spelling-link', href=True):
+        urls.append(a['href'])
+
+    return urls
+
+
+def _cifraclub(artist, song):
+    artist = unidecode.unidecode(artist)
+    song = unidecode.unidecode(song)
+    url = 'https://www.cifraclub.com.br/{}/{}'.format(artist.replace(" ", "-").lower(), song.replace(" ", "-").lower())
+
+    return [url]
+
+# don't even get to this point, but it's an option for source
+# just got to change services_list3 list order
+def _songsterr(artist, song):
+    artist = unidecode.unidecode(artist)
+    song = unidecode.unidecode(song)
+    url = 'http://www.songsterr.com/a/wa/bestMatchForQueryString?s={}&a={}'.format(song, artist)
+    return [url]
